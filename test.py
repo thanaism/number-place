f5b1d32f5ad00aa40311d696be124bf19736d373
thanai-mac <examle@examle.com>
2020-09-16 22:12:48 +0900

    add assertion for "has" function in test.py

--- test.py
+++ test.py
@@ -1,8 +1,20 @@
 from cell_grid import *
 
+cell=Cell(0,0)
+cell.remove(6,7,8)
+assert cell.has(1,2,3,4,5,6,7,8,9)==False
+assert cell.has(1,2,3,4,5,6,7,8,9,AND=False)==True
+assert cell.has(1,2,3,4,5,9)==True
+assert cell.has(1)==True
+assert cell.has(8)==False
+exit()
+
 # インスタンス生成・解答盤面生成
 grid=Grid()
 grid.create()
+
+grid.hidden_single()
+exit()
 
 # 適当に数字を消す
 for i in grid.rand81()[:40]:
@@ -25,3 +37,6 @@ grid.show_grid()
 test_seq='473826519591473286826159734352748691618295473749631852964387125285914367137562948'
 for s in Grid.rotate(test_seq): 
   print(''.join(map(str,s)))
+
+# クラス化していない関数の呼び出し可否確認
+show_index()
